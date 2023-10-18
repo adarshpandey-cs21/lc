413af92a2aa44087ca534c7a0bc254bf24aaba72
Adarsh Kumar Pandey <fakp24@gmail.com>
2023-10-18 14:14:41 +0530

    Time: 2 ms (81.36%), Space: 42.8 MB (58.45%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,32 +1,22 @@
 class Solution {
     public int maxProfit(int k, int[] arr) {
-        
-    int n = arr.length;
+     int n = arr.length;
         // int k=2;
-        int dp[][][] = new int[n][2][k+1];
-        for (int i = 0; i <n ; i++) {
-            for (int j = 0; j <2 ; j++) {
-                for (int l = 0; l < k+1; l++) {
-                    dp[i][j][l]=-1;
+        int dp[][][] = new int[n+1][2][k+1];
+        dp[0][0][k]=dp[0][1][k]=0;
+        for (int idx = n-1; idx >=0 ; idx--) {
+            for (int buy = 0; buy <=1 ; buy++) {
+                for (int cap = k; cap >0 ; cap--) {
+                    int profit=0;
+                    if(buy==1){
+                         profit=Math.max(-arr[idx]+dp[idx+1][0][cap],0+dp[idx+1][1][cap]);
+                    }else{
+                        profit=Math.max(arr[idx]+dp[idx+1][1][cap-1],0+dp[idx+1][0][cap]);
+                    }
+                    dp[idx][buy][cap]=profit;
                 }
             }
         }
-       return maxProfitM(0,1,k,arr,dp);
-    }
-    public static int maxProfitM(int idx,int buy,int cap,int[] arr,int[][][]dp ) {
-        if(cap==0){
-            return 0;
-        }
-        if(idx== arr.length){
-            return 0;
-        }
-        if(dp[idx][buy][cap]!=-1){
-            return dp[idx][buy][cap];
-        }
-        if(buy==1){
-            return dp[idx][buy][cap]=Math.max(-arr[idx]+maxProfitM(idx+1,0,cap,arr,dp),0+maxProfitM(idx+1,1,cap,arr,dp));
-        }else{
-            return dp[idx][buy][cap]=Math.max(arr[idx]+maxProfitM(idx+1,1,cap-1,arr,dp),0+maxProfitM(idx+1,0,cap,arr,dp));
-        }
+        return dp[0][1][k];
     }
 }
